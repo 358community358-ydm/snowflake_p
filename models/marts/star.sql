ba05b8f03263f34dc3d57fb2603337fc62dffcab
morey yogeshh <358community358@gmail.com>
2026-03-08 11:22:44 +0000

    new

---
+++ models/marts/star.sql
@@ -0,0 +1,14 @@
+
+
+{{
+    config(materialized = 'incremental',
+    incremental_strategy = 'merge',
+    unique_key = 'cust_id')
+}}
+
+select l.cust_id,c.first_name,
+l.account_id,l.latest_balance
+from {{ ref('latest_balance') }} l 
+join {{ref('stg_customers')}} c 
+on l.cust_id = c.cust_id
+where latest_balance >5500

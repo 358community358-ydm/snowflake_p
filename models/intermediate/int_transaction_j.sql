539e8d2d10e89b8a147c8e07e7de3cff9c922247
morey yogeshh <358community358@gmail.com>
2026-03-08 08:53:31 +0000

    new

--- models/intermediate/int_transaction_j.sql
+++ models/intermediate/int_transaction_j.sql
@@ -1,11 +1,11 @@
 select
 t.transaction_id,
+a.cust_id,
 t.account_id,
 t.merchant_id,
 t.transaction_type,
 t.transaction_date,
-t.amount,
-a.cust_id 
+t.amount
 from {{ ref('stg_transacition') }} t
 join {{ ref('stg_account')}} a
 on t.account_id = a.account_id
--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,12 +1,12 @@
-{{config(
-    materialized = 'view'
-)}}
+{{
+    config(materialized = 'view')
+}}
 
 select 
 cust_id,
 INITCAP(NULLIF(TRIM(first_name),'')) as first_name,
 INITCAP(NULLIF(TRIM(last_name),''))  as last_name,
 LOWER(NULLIF(TRIM(email),''))         as email,
-CAST(created_at as date)             as created_date
-from {{ source('yogesh', 'customers') }}
+created_at                            as created_date
+from {{source('yogesh','customers')}}  
 where cust_id is not null

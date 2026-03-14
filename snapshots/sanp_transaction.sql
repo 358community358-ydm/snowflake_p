71d026f8cff9eb392bb3e9c86a8dddff2cbd79d8
morey yogeshh <358community358@gmail.com>
2026-03-14 06:30:02 +0000

    new

---
+++ snapshots/sanp_transaction.sql
@@ -0,0 +1,15 @@
+{% snapshot snap_transactions %}
+
+{{
+    config(
+        target_schema='snapshots',
+        unique_key='transaction_id',
+        strategy='timestamp',
+        updated_at='transaction_date'
+    )
+}}
+
+SELECT *
+FROM {{ ref('stg_transacition') }}
+
+{% endsnapshot %}

032b9e814703b49a8b305c2deb62acde385ec29f
358community358-ydm <358community358@gmail.com>
2026-03-14 14:17:57 +0530

    Update sanp_transaction.sql

--- snapshots/sanp_transaction.sql
+++ snapshots/sanp_transaction.sql
@@ -6,14 +6,10 @@
         unique_key='transaction_id',
         strategy='timestamp',
         updated_at='transaction_date'
-        unique_key='TRANSACTION_ID',
-        strategy='timestamp',
-        updated_at='TRANSACTION_DATE'
     )
 }}
 
 SELECT *
-FROM {{ ref('stg_transacition') }}
-FROM {{ source('yogesh','stg_transacition') }}
+FROM {{ ref('stg_transaction') }}
 
-{% endsnapshot %}
+{% endsnapshot %}}

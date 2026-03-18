8437aeb73140a90b1ae86ab81319dfa3f13762e3
358community358@gmail.com <358community358@gmail.com>
2026-03-18 03:22:29 +0000

    new

--- snapshots/snap_customers.sql
+++ snapshots/snap_customers.sql
@@ -5,7 +5,8 @@
         target_schema='snapshots',
         unique_key='cust_id',
         strategy='timestamp',
-        updated_at='created_date'
+        updated_at='created_date',
+         invalidate_hard_deletes=True
     )
 }}
 
